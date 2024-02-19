a44efc4c68bd93a6818f7f483603e254f8f05db0
Adam Perkowski <adas1per@protonmail.com>
2024-02-19 20:25:53 +0100

    skip

--- fetch_cover.sh
+++ fetch_cover.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+clear
 playerctl metadata mpris:artUrl | xargs curl -s | viu -w 50 -
+echo
+playerctl metadata title; playerctl metadata artist
 
--- main.rs
+++ main.rs
@@ -1,10 +1,52 @@
 use std::process::Command;
+use std::io;
+use std::io::Write;
+use std::{thread, time::Duration};
 
 fn main() {
+    bsh();
+    println!();
+
+    let mut inpt = String::new();
+    
+    while inpt != "exit" {
+        inpt = String::new();
+        print!("brink < ");
+
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut inpt).unwrap();
+
+        inpt.pop();
+
+        if inpt == "skip" {
+            Command::new("playerctl")
+                .arg("next")
+                .spawn()
+                .expect("  [ ! ] Failed to connect!");
+
+            thread::sleep(Duration::from_millis(1000));
+
+            bsh();
+            println!("\n  [ * ] Song skipped.\n");
+        }
+        else { println!(); }
+    }
+    
+    clrs();
+    println!("  [ * ] We will miss you...");
+}
+
+fn clrs() {
+    Command::new("clear")
+        .spawn()
+        .expect("  [ ! ] Failed to connect!");
+}
+
+fn bsh() {
     Command::new("bash")
         .arg("fetch_cover.sh")
         .spawn()
-        .expect("ls command failed to start")
+        .expect("  [ ! ] Failed to connect!")
         .wait()
         .unwrap();
 }
